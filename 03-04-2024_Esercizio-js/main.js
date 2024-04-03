7565605ad9d07a2efb4a4dbb7e777bbc42bb7921
Fortunato Cusati <fortunato_cusati@yahoo.it>
2024-04-03 19:22:00 +0200

    push esercizi

---
+++ 03-04-2024_Esercizio-js/main.js
@@ -0,0 +1,11 @@
+let num1 = 10
+let num2 = 5
+
+console.log ("Il valore della variabile num1 è " + num1)
+console.log ("Il valore della variabile num2 è " + num2)
+
+let stringa = "un'altra stringa"
+console.log ("Il nuovo valore della variabile stringa è " + stringa)
+
+const PIGRECO = 20 
+console.log ("Il valore di PIGRECO è " + PIGRECO)
