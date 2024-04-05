832ecfd175bde3bfb3d19a4de04cf0ddd1ae22e7
Fortunato Cusati <fortunato_cusati@yahoo.it>
2024-04-05 19:08:06 +0200

    push esercizi nuovi

---
+++ 05-04-2024_Esercizio2/main.js
@@ -0,0 +1,49 @@
+// Traccia 1
+
+let num1 = 2;
+let num2 = 5;
+let num3 = 7;
+let num4 = 8;
+let num5 = 17;
+
+let somma = num1 + num2 + num3 + num4 + num5;
+console.log("La somma tra i numeri equivale a " + somma);
+
+let media = somma / 5;
+console.log("La media equivale a " + media);
+
+console.log("===============================================");
+
+// Traccia 2
+
+let anno_corrente = 2024;
+anno_di_nascita = prompt("Inserisci il tuo anno di nascita");
+
+let anni_attuali = anno_corrente - parseInt(anno_di_nascita);
+let anni_per_i_cento = 100 - anni_attuali;
+
+
+console.log("Hai " + anni_attuali + " anni, e ti mancano " + anni_per_i_cento + " anni per arrivare ai 100!");
+
+console.log("===============================================");
+
+
+// Traccia 3
+
+num1 = prompt("inserisci il primo numero");
+num2 = prompt("Inserisci il secondo numero");
+
+let addizione = parseInt(num1) + parseInt(num2);
+let sottrazione = num1 - num2;
+let moltiplicazione = num1 * num2;
+let divisione = num1 / num2;
+let potenza = num1 ** num2;
+
+console.log("Con i numeri da te scelti, i risultati delle varie operazioni sono:");
+console.log("Somma = " + addizione);
+console.log("Differenza = " + sottrazione);
+console.log("Moltiplicazione = " + moltiplicazione);
+console.log("Divisione = " + divisione);
+console.log("Potenza = " + potenza);
+
+console.log("===============================================");
