f09789974f5f39f8d1072bfcea550d044e9ec37f
Fortunato Cusati <fortunato_cusati@yahoo.it>
2024-05-13 19:12:06 +0200

    modificato file js riducendo i set interval come suggerito dal coach

--- 06-05-2024_Presto.it/js/main.js
+++ 06-05-2024_Presto.it/js/main.js
@@ -6,60 +6,36 @@ let ville = document.querySelector('#ville');
 
 // Animazione Annunci 
 
-let counter_annunci = 0;
+function crea_intervallo(elemento, numMax, tempo) {
+    let counter = 0;
+    let intervallo = setInterval(() => {
+        if (counter < numMax) {
+            counter++;
+            elemento.innerText = counter;
+        } else {
+            clearInterval(intervallo);
+        }
+    }, tempo);
+}
 
 let osserva_annunci = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting){
-            let intervallo1 = setInterval(() => {
-                if (counter_annunci < 135) {
-                    counter_annunci ++;
-                    annunci.innerText = counter_annunci;
-                } else {
-                    clearInterval(intervallo1);
-                }
-            }, 10)
+            crea_intervallo(annunci, 135, 8);
+            crea_intervallo(clienti, 200, 5);
+            crea_intervallo(ville, 100, 8)  
         }
     })
 });
-osserva_annunci.observe(annunci);
 
-// Animazione Clienti 
+osserva_annunci.observe(ville);
 
-let counter_clienti = 0;
 
-let osserva_clienti= new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting){
-            let intervallo1 = setInterval(() => {
-                if (counter_clienti < 200) {
-                    counter_clienti ++;
-                    clienti.innerText = counter_clienti;
-                } else {
-                    clearInterval(intervallo1);
-                }
-            }, 8)
-        }
-    })
-});
-osserva_clienti.observe(clienti);
 
-// Animazione Ville vendute 
 
-let counter_ville = 0;
 
-let osserva_ville = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting){
-            let intervallo1 = setInterval(() => {
-                if (counter_ville < 100) {
-                    counter_ville ++;
-                    ville.innerText = counter_ville;
-                } else {
-                    clearInterval(intervallo1);
-                }
-            }, 15)
-        }
-    })
-});
-osserva_ville.observe(ville);
+
+
+
+
+
